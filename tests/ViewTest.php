786db609efc8406a671adc0f09c7647c830327ff
John P. Bloch <johnpbloch@gmail.com>
2013-01-12 15:24:57 -0500

    Data provider and tests for the test_set method

--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -4,20 +4,37 @@ include_once __DIR__ . '/mocks/View.php';
 
 class ViewTest extends PHPUnit_Framework_TestCase
 {
-	
+
 	public function setUp()
 	{
 		$this->view = new View('default');
 	}
 
-	public function test_set()
+	/**
+	 * @dataProvider set_provider
+	 */
+	public function test_set($data)
 	{
-
+		$this->view->set($data);
+		foreach($data as $k => $v)
+		{
+			$this->assertObjectHasAttribute($k, $this->view);
+			$this->assertEquals($v, $this->view->$k);
+		}
 	}
-	
+
+	public function set_provider()
+	{
+		return array( array( array(
+					'a' => 'z',
+					'b' => 'y',
+					'c' => 'z'
+				)));
+	}
+
 	public function test___to_string()
 	{
-		
+
 	}
 
 }
