74432d967932f168986df707f65a7fb18e98711e
John P. Bloch <johnpbloch@gmail.com>
2013-01-12 15:07:32 -0500

    Get the mock class and create the object in setup

--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -1,7 +1,14 @@
 <?php
 
+include_once __DIR__ .'/mock/View.php';
+
 class ViewTest extends PHPUnit_Framework_TestCase
 {
+	
+	public public function setUp()
+	{
+		$this->view = new View('default');
+	}
 
 	public function test_set()
 	{
