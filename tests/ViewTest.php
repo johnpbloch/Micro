528cd194ff38b8150be20aab654c36ba5e70eecb
John P. Bloch <johnpbloch@gmail.com>
2013-01-12 21:23:39 -0500

    Run the parent's setup method

--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -1,12 +1,11 @@
 <?php
 
-include_once __DIR__ . '/mocks/View.php';
-
 class ViewTest extends PHPUnit_Framework_TestCase
 {
 
 	public function setUp()
 	{
+		parent::setUp();
 		$this->view = new View('default');
 	}
 
